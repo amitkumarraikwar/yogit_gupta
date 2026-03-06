a29a6b8583d7a2def6dec50881ad6f46dd2ee7a5
Amit Kumar Raikwar <amitkumarraikwar92@gmail.com>
2026-03-06 12:44:07 +0530

    fix: suppress hydration warning caused by browser extensions

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${playfair.variable} ${poppins.variable} font-sans`}>
         {children}
       </body>
